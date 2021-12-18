2a9396663d9f6579f657de2b8cf93ad955d72bfe
Kareem Elantably <kelantably2019@gmail.com>
2021-12-19 01:01:34 +0200

    btn_nav_bar,items

---
+++ lib/screens/home.dart
@@ -0,0 +1,24 @@
+import 'package:exchange_app/statefull_widgets/nav_bar_widget.dart';
+import 'package:flutter/material.dart';
+import '../stateless_widgets/bar.dart';
+class home extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: startDrawer(),
+      appBar: BaseAppBar(
+        backgroundColor: Colors.red,
+          title: Text('title git'),
+          appBar: AppBar(),
+          ),
+      body: SafeArea(
+        child: ElevatedButton(
+          onPressed: (){Navigator.pushNamed(context, '/items');},
+          child: Text('cat'),
+          ),
+          ),
+          bottomNavigationBar: nav_bar_widget(),
+    );
+  }
+}
---
+++ lib/statefull_widgets/nav_bar_widget.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class nav_bar_widget extends StatefulWidget {
+  const nav_bar_widget({Key? key}) : super(key: key);
+
+  @override
+  State<nav_bar_widget> createState() => _nav_bar_widget();
+}
+
+class _nav_bar_widget extends State<nav_bar_widget> {
+  
+  int _selectedIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+      if (_selectedIndex==0){
+        Navigator.pushNamed(context, '/');
+      }
+      else if(_selectedIndex==1){
+        Navigator.pushNamed(context, '/mmm');
+      }
+      else{
+        
+      }
+      
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+            
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      
+    );
+  }
+}
