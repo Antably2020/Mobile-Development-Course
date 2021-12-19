bf3677dfd3934ad6c4fcf9bc6eceaaa3cfd6b905
Kareem Elantably <kelantably2019@gmail.com>
2021-12-19 18:15:08 +0200

    Update offers.dart

--- lib/models/offers.dart
+++ lib/models/offers.dart
@@ -9,13 +9,13 @@ class Offers {
 }
 
 List<Offers> allOffers = [
-  Offers("Car", "Car", "Pending", "mob.png", "laptop.jpg"),
-  Offers("Toy", "Toy", "accepted", "shoes.png", "toy.jpg"),
+  Offers("Mobile", "Car", "Pending", "mob.png", "laptop.jpg"),
+  Offers("Addidas shoes", "Toy", "accepted", "shoes.png", "toy.jpg"),
   Offers("Guitar", "Mobile", "Pending", "guitar.png", "mob.png"),
-   Offers("Shoes", "Shoes", "rejected", "mob.png", "shoes.png"),
+   Offers("Mobile", "Shoes", "rejected", "mob.png", "shoes.png"),
   Offers("Guitar", "Mobile", "Pending", "guitar.png", "mob.png"),
-  Offers("Toy", "Toy", "accepted", "shoes.png", "toy.jpg"),
-    Offers("Toy", "Toy", "accepted", "shoes.png", "toy.jpg"),
+  Offers("Nike shoes", "Toy", "accepted", "shoes.png", "toy.jpg"),
+    Offers("shoes", "Toy", "accepted", "shoes.png", "toy.jpg"),
 
 
 ];
