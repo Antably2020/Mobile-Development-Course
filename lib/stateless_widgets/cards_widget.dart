887db5f41fb37398fb9f38aa8ded306bc9e8cb11
mahmoud zoair <73954049+mahmoudzoair@users.noreply.github.com>
2021-12-19 17:43:49 +0200

    Update cards_widget.dart

--- lib/stateless_widgets/cards_widget.dart
+++ lib/stateless_widgets/cards_widget.dart
@@ -37,7 +37,7 @@ class _CardSquare extends State<CardSquare> {
                 Container(
                   padding: EdgeInsets.all(20),
                  
-                   height: 120,
+                   height: 140,
                    width: 160,
                   decoration: BoxDecoration(
                     color: items[index].color,
