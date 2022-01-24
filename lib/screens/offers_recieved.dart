30dcdb69db46a365f86a4298e3a5dabe601fd87e
mahmoud zoair <73954049+mahmoudzoair@users.noreply.github.com>
2022-01-24 07:06:59 +0200

    z

--- lib/screens/offers_recieved.dart
+++ lib/screens/offers_recieved.dart
@@ -2,6 +2,22 @@ import 'package:exchange_app/statefull_widgets/nav_bar_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:exchange_app/models/offers.dart';
 
+
+
+
+
+
+//not needed screen
+
+
+
+
+
+
+
+
+
+
 class offers_recieved extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
