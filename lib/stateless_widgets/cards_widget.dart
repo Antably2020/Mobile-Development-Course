1bc4a598ccc1a3e5e9934c78524bfd2faf1eed8f
Kareem Elantably <kelantably2019@gmail.com>
2022-01-26 14:08:19 +0200

    Update cards_widget.dart

--- lib/stateless_widgets/cards_widget.dart
+++ lib/stateless_widgets/cards_widget.dart
@@ -69,14 +69,13 @@ class _CardSquare extends State<CardSquare> {
                   children: <Widget>[
                     Container(
                       child: InkWell(
-                        onTap: () async {
-                          //Navigator.of(context).push(MaterialPageRoute(
-                             //builder: (context) => AllItems(
-                                  // title: 'Vehicles',
-                                   await authService.signout();
-                                   Navigator.pushNamed(context, "/");
-                                  
-                           
+                        onTap: () async{
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => AllItems(
+                                    title: 'Vehicles',
+                                  )));
+                              //await authService.signout();
+                                //           Navigator.pushNamed(context, '/');
                         },
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.center,
